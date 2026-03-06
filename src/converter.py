84e49bc469df73a2973a14543ed91d6d4c4ea7ad
Xenia <>
2026-03-06 11:21:18 +0100

    Conversion text to textnodes implemented

--- src/converter.py
+++ src/converter.py
@@ -90,5 +90,12 @@ def split_nodes_link(old_nodes):
             new_nodes.append(TextNode(text, TextType.TEXT))
     return new_nodes
 
-
+def text_to_textnodes(text):
+    text_nodes = [TextNode(text, TextType.TEXT)]
+    text_nodes = split_nodes_image(text_nodes)
+    text_nodes = split_nodes_link(text_nodes)
+    text_nodes = split_nodes_delimiter(text_nodes, "**", TextType.BOLD)
+    text_nodes = split_nodes_delimiter(text_nodes, "_", TextType.ITALIC)
+    text_nodes = split_nodes_delimiter(text_nodes, "`" , TextType.CODE)
+    return text_nodes
 
--- src/test_converter.py
+++ src/test_converter.py
@@ -226,9 +226,28 @@ class text_split_nodes_links(unittest.TestCase):
                     TextNode(" are good tests.", TextType.TEXT)]
         self.assertEqual(split_nodes_link([node_1, node_2, node_3]), expected)
 
+class test_text_to_textnodes(unittest.TestCase):
 
+    def test_empty_text(self):
+        text = ""
+        expected = [TextNode("", TextType.TEXT)]
+        self.assertEqual(text_to_textnodes(text), expected)
 
-
+    def test_find_all(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        expected = [
+                        TextNode("This is ", TextType.TEXT),
+                        TextNode("text", TextType.BOLD),
+                        TextNode(" with an ", TextType.TEXT),
+                        TextNode("italic", TextType.ITALIC),
+                        TextNode(" word and a ", TextType.TEXT),
+                        TextNode("code block", TextType.CODE),
+                        TextNode(" and an ", TextType.TEXT),
+                        TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                        TextNode(" and a ", TextType.TEXT),
+                        TextNode("link", TextType.LINK, "https://boot.dev"),
+                    ]
+        self.assertEqual(text_to_textnodes(text), expected)
 
 
 if __name__ == "__main__":
