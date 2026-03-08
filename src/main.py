648e6491c54ea39bbc650bde8b84393a8ae86fef
Xenia <>
2026-03-08 20:33:41 +0100

    functionality for multiple page implemented

--- src/main.py
+++ src/main.py
@@ -15,14 +15,27 @@ def copy_dictree(source, target):
             os.mkdir(os.path.join(target, file))
             copy_dictree(path, os.path.join(target, file))
             
-
-
+def generate_pages_recursive(dir_content, template, dir_destination):
+    content_content = os.listdir(dir_content)
+    content_destination = os.listdir(dir_destination)
+    for file in content_content:
+        if file in content_destination:
+            continue
+        path = os.path.join(dir_content, file)
+        if not os.path.isfile(path):
+            os.mkdir(os.path.join(dir_destination, file))
+            generate_pages_recursive(path, template, os.path.join(dir_destination, file))
+            continue
+        if file.endswith(".md"):
+            generate_page(path, template, os.path.join(dir_destination, file[:-3]+".html"))
+        
 def main():
     if os.path.exists("./public"):
         shutil.rmtree("./public")
     os.mkdir("./public")
     copy_dictree("./static", "./public")
 
-    generate_page("content/index.md", "template.html", "public/index.html")
-
+    generate_pages_recursive("./content", "template.html", "./public")
+    #generate_page("./content/index.md", template, "./public/index.html"))
+    
 main()
